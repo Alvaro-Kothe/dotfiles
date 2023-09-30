100ae171ec549ed832123dbea845817e9bdb6bf9
Alvaro-Kothe <kothe65@gmail.com>
2023-09-30 13:18:19 -0300

    Merge git plugins in one file
    - Change git keymaps (use keymaps provided by lazyvim);
    - Add new keymap to show git status <space>gs;

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,17 +1,10 @@
 return {
-  -- NOTE: First, some plugins that don't require any configuration
-
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-
-
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',          opts = {} },
+  { 'folke/which-key.nvim', opts = {} },
 
   {
     -- Set lualine as statusline
@@ -39,5 +32,5 @@ return {
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',         opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
 }
