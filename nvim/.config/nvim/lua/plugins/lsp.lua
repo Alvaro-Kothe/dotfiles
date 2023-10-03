1c1abf69165ab18d3bcb3599888f8b5a1a4a4c92
Alvaro-Kothe <kothe65@gmail.com>
2023-10-03 15:36:04 -0300

    Use conform to format with opt out auto formatting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -69,9 +69,9 @@ function M.config()
     end, '[W]orkspace [L]ist Folders')
 
     -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-      vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
+  --   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  --     vim.lsp.buf.format()
+  --   end, { desc = 'Format current buffer with LSP' })
   end
 
   -- Enable the following language servers
