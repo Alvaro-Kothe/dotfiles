34fb2b95d1555cdbafeb2c61d55e96364b2ef34b
Alvaro-Kothe <kothe65@gmail.com>
2024-03-18 09:21:47 -0300

    Update

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,76 +1,76 @@
-local M = {
-  -- Autocompletion
+local M = { -- Autocompletion
   "hrsh7th/nvim-cmp",
-  lazy = true,
+  event = "InsertEnter",
   dependencies = {
-    "onsails/lspkind-nvim",
     -- Snippet Engine & its associated nvim-cmp source
-    "L3MON4D3/LuaSnip",
+    {
+      "L3MON4D3/LuaSnip",
+      build = "make install_jsregexp",
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        --    See the README about individual language/framework/plugin snippets:
+        --    https://github.com/rafamadriz/friendly-snippets
+        {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
+        },
+      },
+    },
     "saadparwaiz1/cmp_luasnip",
 
-    -- Adds LSP completion capabilities
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-
-    -- Adds a number of user-friendly snippets
-    "rafamadriz/friendly-snippets",
-
-    -- Extra
     "micangl/cmp-vimtex",
   },
 }
 
-function M.config()
-  -- Set completeopt to have a better completion experience
-  vim.opt.completeopt = { "menu", "menuone", "noselect" }
+M.config = function()
+  -- See `:help cmp`
   local cmp = require("cmp")
-  local lspkind = require("lspkind")
   local luasnip = require("luasnip")
-  require("luasnip.loaders.from_vscode").lazy_load()
   luasnip.config.setup({})
 
   cmp.setup({
-    formatting = {
-      format = lspkind.cmp_format({
-        mode = "symbol", -- show only symbol annotations
-        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-        -- can also be a function to dynamically calculate max width such as
-        -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-        ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-        -- The function below will be called before any actual modifications from lspkind
-        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-        before = function(_, vim_item)
-          return vim_item
-        end,
-      }),
-    },
     snippet = {
       expand = function(args)
         luasnip.lsp_expand(args.body)
       end,
     },
+    completion = { completeopt = "menu,menuone,noinsert" },
+
+    -- For an understanding of why these mappings were
+    -- chosen, you will need to read `:help ins-completion`
+    --
+    -- No, but seriously. Please read `:help ins-completion`, it is really good!
     mapping = cmp.mapping.preset.insert({
+      -- Select the [n]ext item
       ["<C-n>"] = cmp.mapping.select_next_item(),
+      -- Select the [p]revious item
       ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+      -- scroll the documentation window [d]own / [f]orward
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete({}),
-      ["<CR>"] = cmp.mapping({
-        i = function(fallback)
-          if cmp.visible() and cmp.get_active_entry() then
-            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-          else
-            fallback()
-          end
-        end,
-        s = cmp.mapping.confirm({ select = true }),
-        -- c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-      }),
+
+      -- Accept ([y]es) the completion.
+      --  This will auto-import if your LSP supports it.
+      --  This will expand snippets if the LSP sent a snippet.
       ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+      -- Manually trigger a completion from nvim-cmp.
+      --  Generally you don't need this, because nvim-cmp will display
+      --  completions whenever it has completion options available.
+      ["<C-Space>"] = cmp.mapping.complete({}),
+
+      -- Think of <c-l> as moving to the right of your snippet expansion.
+      --  So if you have a snippet that's like:
+      --  function $name($args)
+      --    $body
+      --  end
+      --
       -- <c-l> will move you to the right of each of the expansion locations.
       -- <c-h> is similar, except moving you backwards.
       ["<C-l>"] = cmp.mapping(function()
@@ -83,16 +83,17 @@ function M.config()
           luasnip.jump(-1)
         end
       end, { "i", "s" }),
-    }),
 
-    sources = cmp.config.sources({
+      -- For more advanced luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+      --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+    }),
+    sources = {
       { name = "nvim_lsp" },
+      { name = "buffer" },
       { name = "luasnip" }, -- For luasnip users.
       { name = "vimtex" },
       { name = "path" },
-    }, {
-      { name = "buffer" },
-    }),
+    },
   })
 end
 
