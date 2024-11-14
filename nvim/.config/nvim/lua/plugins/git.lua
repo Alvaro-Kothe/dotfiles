b42273e84142f563e10ee4eae59dc0d8dac8a621
Alvaro-Kothe <kothe65@gmail.com>
2024-11-14 14:41:04 -0300

    feat: Add Snacks

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    event = "VeryLazy",
     opts = {
       on_attach = function(buffer)
         local gitsigns = require("gitsigns")
