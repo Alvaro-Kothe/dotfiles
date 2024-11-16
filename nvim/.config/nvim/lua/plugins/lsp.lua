3eda8daad8eb2a0406a98251864b339d7367f5df
Alvaro-Kothe <kothe65@gmail.com>
2024-11-16 16:49:27 -0300

    refactor: cmp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -15,9 +15,6 @@ local M = {
       "j-hui/fidget.nvim",
       opts = {},
     },
-
-    -- Additional lua configuration, makes nvim stuff amazing!
-    { "folke/neodev.nvim", opts = {} },
   },
 }
 
