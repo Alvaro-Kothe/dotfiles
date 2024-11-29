be631fa22f4aebbff2d693f4972f0cbb0986c390
Alvaro-Kothe <kothe65@gmail.com>
2024-11-29 12:54:55 -0300

    fix: disable autopairs for roxygen2 comments

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -3,11 +3,20 @@ return {
   event = "InsertEnter",
   -- Optional dependency
   dependencies = { "hrsh7th/nvim-cmp" },
-  config = function()
-    require("nvim-autopairs").setup({})
+  opts = {},
+  config = function(_, opts)
+    local npairs = require("nvim-autopairs")
+    npairs.setup(opts)
+
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
     local cmp = require("cmp")
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+    local Rule = require("nvim-autopairs.rule")
+    local cond = require("nvim-autopairs.conds")
+
+    -- Disable autopairs for roxygen2 docs prefix
+    npairs.get_rules("'")[1]:with_pair(cond.not_before_text("#"))
   end,
 }
