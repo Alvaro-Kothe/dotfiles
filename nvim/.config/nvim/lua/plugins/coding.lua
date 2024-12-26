9838f09cb39096d082c48ff9ded2631122253bf8
Alvaro-Kothe <kothe65@gmail.com>
2024-12-26 15:24:24 -0300

    fix(cmp-buffer): use max file size of 1MiB

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -99,7 +99,19 @@ return {
           { name = "luasnip" },
           { name = "path" },
         }, {
-          { name = "buffer" },
+          {
+            name = "buffer",
+            option = {
+              get_bufnrs = function()
+                local buf = vim.api.nvim_get_current_buf()
+                local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+                if byte_size > 1024 * 1024 then -- 1 Megabyte max
+                  return {}
+                end
+                return { buf }
+              end,
+            },
+          },
         }),
         formatting = {
           format = lspkind.cmp_format({
