9956ea19f5f5d1e8f1f11e48a6a2481d9433f433
Alvaro-Kothe <kothe65@gmail.com>
2024-12-28 10:10:57 -0300

    feat(snippets)!: replace luasnip for nvim-snippets

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -17,25 +17,7 @@ return {
         },
       },
       { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-      {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-          {
-            "rafamadriz/friendly-snippets",
-            config = function()
-              require("luasnip.loaders.from_vscode").lazy_load({
-                override_priority = 100,
-              })
-            end,
-          },
-        },
-        build = "make install_jsregexp",
-        opts = {},
-        config = function(_, opts)
-          require("luasnip").config.setup(opts)
-          require("luasnip.loaders.from_lua").lazy_load()
-        end,
-      },
+      "garymjr/nvim-snippets",
       "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-nvim-lsp",
@@ -44,12 +26,8 @@ return {
     },
     opts = function()
       local cmp = require("cmp")
-      local luasnip = require("luasnip")
       local lspkind = require("lspkind")
       return {
-        snippet = {
-          expand = function(args) luasnip.lsp_expand(args.body) end,
-        },
         completion = { completeopt = "menu,menuone,noinsert" },
 
         -- For an understanding of why these mappings were
@@ -76,27 +54,12 @@ return {
           --  completions whenever it has completion options available.
           ["<C-Space>"] = cmp.mapping.complete({}),
 
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          ["<C-l>"] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then luasnip.expand_or_jump() end
-          end, { "i", "s" }),
-          ["<C-h>"] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then luasnip.jump(-1) end
-          end, { "i", "s" }),
-
           -- For more advanced luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" },
+          { name = "snippets" },
           { name = "path" },
         }, {
           {
@@ -127,7 +90,7 @@ return {
             show_labelDetails = true, -- show labelDetails in menu. Disabled by default
             menu = {
               nvim_lsp = "[LSP]",
-              luasnip = "[Snip]",
+              snippets = "[snippets]",
               path = "[Path]",
               buffer = "[Buffer]",
               cmp_r = "[R]",
@@ -137,6 +100,50 @@ return {
         },
       }
     end,
+  },
+  {
+    "garymjr/nvim-snippets",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    opts = {
+      friendly_snippets = true,
+    },
+    keys = {
+      {
+        "<C-l>",
+        function()
+          if vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function() vim.snippet.jump(1) end)
+            return
+          end
+          return "<C-l>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      {
+        "<C-l>",
+        function()
+          vim.schedule(function() vim.snippet.jump(1) end)
+        end,
+        expr = true,
+        silent = true,
+        mode = "s",
+      },
+      {
+        "<C-h>",
+        function()
+          if vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function() vim.snippet.jump(-1) end)
+            return
+          end
+          return "<C-h>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+    },
   },
   { -- optional cmp completion source for require statements and module annotations
     "hrsh7th/nvim-cmp",
