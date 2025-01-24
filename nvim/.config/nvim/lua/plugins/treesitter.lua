35442572bd313e1a8568378860cfeb5ea0a3fdbe
Alvaro-Kothe <kothe65@gmail.com>
2025-01-24 13:44:38 -0300

    fix!: treesitter foldexpr

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -44,8 +44,8 @@ return {
     },
     config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
     init = function()
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      -- vim.opt.foldmethod = "expr"
+      -- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     end,
   },
   {
