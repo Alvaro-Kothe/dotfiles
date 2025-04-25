08c723c8bc970d9df428cbb709b759ac242f4aa0
Alvaro-Kothe <kothe65@gmail.com>
2025-04-25 08:09:04 -0300

    feat: use render-markdown

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -9,4 +9,28 @@ return {
       vim.fn["mkdp#util#install"]()
     end,
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    version = "8.3.x",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = function() return require("render-markdown.state").enabled end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      }):map("<leader>um")
+    end,
+  },
 }
