c3544047c9902a7f941548cfbb5673a81f61c5a5
Álvaro Kothe <kothe65@gmail.com>
2025-08-12 09:20:10 -0300

    feat: add bash config

---
+++ bash/.bashrc.d/01-functions.sh
@@ -0,0 +1,30 @@
+add_file() {
+    [ -f $1 ] && . $1
+}
+
+path_add () {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        PATH="$1${PATH:+":$PATH"}"
+    fi
+}
+
+projtmux () {
+  local proj_dir=$(find ~/git ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
+
+  # Quit if directory doesn't exist
+  [[ -d "$proj_dir" ]] || return
+
+  local proj_name=$(basename "$proj_dir")
+
+  if ! tmux has-session -t "$proj_name" 2>/dev/null; then
+    # Create a new session if it doesn't exist
+    tmux new-session -d -s "$proj_name" -c "$proj_dir"
+  fi
+
+  # Attach or switch to the session based on whether we're already in tmux
+  if [[ -n "$TMUX" ]]; then
+    tmux switch-client -t "$proj_name"
+  else
+    tmux attach-session -t "$proj_name"
+  fi
+}
---
+++ bash/.bashrc.d/20-exports.sh
@@ -0,0 +1,7 @@
+export MANPAGER='nvim +Man!'
+export MANWIDTH=999
+
+path_add "$HOME/bin"
+path_add "${GOPATH:-$HOME/go}/bin"
+path_add "$HOME/.cargo/bin"
+path_add "$HOME/.local/bin"
