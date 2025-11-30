9406c3fbc0d1262f46a62f6976d9b8fc7892357d
Alvaro-Kothe <kothe65@gmail.com>
2025-11-29 21:38:29 -0300

    feat: gdb debugger

--- nvim/.config/nvim/lua/plugins/debugger.lua
+++ nvim/.config/nvim/lua/plugins/debugger.lua
@@ -55,6 +55,29 @@ return {
         desc = "Log message",
       },
     },
+    config = function(_, opts)
+      local dap = require("dap")
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+      }
+
+      dap.configurations.c = {
+        {
+          name = "Launch",
+          type = "gdb",
+          request = "launch",
+          program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+          args = {}, -- provide arguments if needed
+          cwd = "${workspaceFolder}",
+          stopAtBeginningOfMainSubprogram = false,
+        },
+      }
+
+      dap.configurations.cpp = dap.configurations.c
+      dap.configurations.rust = dap.configurations.c
+    end,
   },
   {
     "rcarriga/nvim-dap-ui",
