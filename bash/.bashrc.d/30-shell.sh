dffe9184880b4f4b1bdb91158d9a7a9f04167eef
Alvaro-Kothe <kothe65@gmail.com>
2026-05-30 13:59:12 -0300

    fix(bash): stop using starship for prompt

--- bash/.bashrc.d/30-shell.sh
+++ bash/.bashrc.d/30-shell.sh
@@ -1,5 +1,8 @@
-if [[ $(command -v starship) ]]; then
-    eval "$(starship init bash)"
+if [[ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]]; then
+  source /usr/share/git-core/contrib/completion/git-prompt.sh
+  export GIT_PS1_SHOWDIRTYSTATE=1
+  export PROMPT_DIRTRIM=3
+  export PS1='\[\e[1;36m\]\w\[\e[0;33m\]$(__git_ps1 " (%s)")\[\e[1;32m\] \$\[\e[0m\] '
 fi
 
 if [[ $(command -v fzf) ]]; then
